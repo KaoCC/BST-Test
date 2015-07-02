9f3e518e6cf501bb87bc1bb5d55d0cb7eabd40a0
Chih-Chen Kao <elros.morlin@gmail.com>
2015-07-02 22:49:15 +0800

    HSA init

---
+++ hsa_bst/main.cpp
@@ -0,0 +1,258 @@
+/** 
+ * Copyright (c) 2015 VCO Lab CSIE department of National Taiwan University All
+ * rights reserved.
+ * 
+ * This file is part of OpenCL Binary Search Tree for HSA comparison
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+
+#include <CL/cl.h>
+
+#include "bst_cpu.hpp"
+#include "profile_timer.hpp"
+#include "runtime_exception.hpp"
+#include "opencl_factory.hpp"
+
+#define BST_NODE_COUNT      (1024 * 4096)
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    runtime_exception::register_signal_handlers();
+    
+    BSTNode* node_list = new BSTNode[BST_NODE_COUNT];
+    for (int i = 0; i < BST_NODE_COUNT; i++) {
+        node_list[i].data.key   = rand() % BST_NODE_COUNT;
+        node_list[i].data.value = i;
+    }
+    
+    start_timer();
+    bst_build(node_list, BST_NODE_COUNT);
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST build time: %lld\n", dump_timer_delta());
+    
+    int* find_key_list    = new int[BST_NODE_COUNT];
+    int* find_result_list = new int[BST_NODE_COUNT];
+    
+    /**
+     * GPU OpenCL random traverse.
+     */
+    for (int i = 0; i < BST_NODE_COUNT; i++) {
+        find_key_list[i] = rand() % BST_NODE_COUNT;
+    }
+    
+    start_timer();
+    for (int i = 0; i < BST_NODE_COUNT; i++) {
+        bst_find(node_list, BST_NODE_COUNT, &find_result_list[i], find_key_list[i]);
+    }
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST CPU search time: %lld\n", dump_timer_delta());
+    
+    initialize_cl_environment();
+    load_cl_program_from_file("bst_kernel.cl");
+
+    start_timer();
+    // <editor-fold defaultstate="collapsed" desc="clCreateBuffer">
+    cl_mem cl_node_list = clCreateBuffer(context, CL_MEM_READ_ONLY,
+                                         sizeof (BSTNode) * BST_NODE_COUNT,
+                                         nullptr, nullptr);
+    cl_mem cl_find_key_list = clCreateBuffer(context, CL_MEM_READ_ONLY,
+                                             sizeof (int) * BST_NODE_COUNT,
+                                             nullptr, nullptr);
+    cl_mem cl_find_result_list = clCreateBuffer(context, CL_MEM_READ_WRITE,
+                                                sizeof (int) * BST_NODE_COUNT,
+                                                nullptr, nullptr);
+    // </editor-fold>
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST GPU clCreateBuffer time: %lld\n", dump_timer_delta());
+    
+    start_timer();
+    // <editor-fold defaultstate="collapsed" desc="clEnqueueWriteBuffer">
+    error = clEnqueueWriteBuffer(command_queue, cl_node_list, CL_TRUE, 0,
+                                 sizeof (BSTNode) * BST_NODE_COUNT, node_list,
+                                 0, nullptr, nullptr);
+    error = clEnqueueWriteBuffer(command_queue, cl_find_key_list, CL_TRUE, 0,
+                                 sizeof (int) * BST_NODE_COUNT, find_key_list,
+                                 0, nullptr, nullptr);
+    // </editor-fold>
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST GPU clEnqueueWriteBuffer time: %lld\n", dump_timer_delta());
+    
+    int node_count = BST_NODE_COUNT;
+    error = clSetKernelArg(kernel, 0, sizeof(cl_mem), &cl_node_list);
+    error = clSetKernelArg(kernel, 1, sizeof(int),    &node_count);
+    error = clSetKernelArg(kernel, 2, sizeof(cl_mem), &cl_find_result_list);
+    error = clSetKernelArg(kernel, 3, sizeof(cl_mem), &cl_find_key_list);
+    error = clSetKernelArg(kernel, 4, sizeof(int),    &node_count);
+    
+    size_t global_size, local_size;
+    global_size = node_count;
+    error = clGetKernelWorkGroupInfo(kernel, device_id, 
+                                     CL_KERNEL_WORK_GROUP_SIZE, 
+                                     sizeof(local_size), &local_size, nullptr);
+    local_size = min(local_size, global_size);
+    
+    start_timer();
+    // <editor-fold defaultstate="collapsed" desc="clEnqueueNDRangeKernel">
+    error = clEnqueueNDRangeKernel(command_queue, kernel, 1, nullptr,
+                                   &global_size, &local_size, 0, nullptr,
+                                   nullptr);
+    clFinish(command_queue);
+    // </editor-fold>
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST GPU clEnqueueNDRangeKernel time: %lld\n", dump_timer_delta());
+    
+    start_timer();
+    // <editor-fold defaultstate="collapsed" desc="clEnqueueReadBuffer">
+    error = clEnqueueReadBuffer(command_queue, cl_find_result_list, CL_TRUE, 0,
+                                sizeof (int) * BST_NODE_COUNT, find_result_list,
+                                0, nullptr, nullptr);
+    // </editor-fold>
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST GPU clEnqueueReadBuffer time: %lld\n", dump_timer_delta());
+
+    /**
+     * GPU OpenCL no branch traverse.
+     */
+    for (int i = 0; i < BST_NODE_COUNT; i++) {
+        find_key_list[i] = BST_NODE_COUNT / 2;
+    }
+    
+    start_timer();
+    // <editor-fold defaultstate="collapsed" desc="clEnqueueWriteBuffer">
+    error = clEnqueueWriteBuffer(command_queue, cl_node_list, CL_TRUE, 0,
+                                 sizeof (BSTNode) * BST_NODE_COUNT, node_list,
+                                 0, nullptr, nullptr);
+    error = clEnqueueWriteBuffer(command_queue, cl_find_key_list, CL_TRUE, 0,
+                                 sizeof (int) * BST_NODE_COUNT, find_key_list,
+                                 0, nullptr, nullptr);
+    // </editor-fold>
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST GPU clEnqueueWriteBuffer time: %lld\n", dump_timer_delta());
+    
+    node_count = BST_NODE_COUNT;
+    error = clSetKernelArg(kernel, 0, sizeof(cl_mem), &cl_node_list);
+    error = clSetKernelArg(kernel, 1, sizeof(int),    &node_count);
+    error = clSetKernelArg(kernel, 2, sizeof(cl_mem), &cl_find_result_list);
+    error = clSetKernelArg(kernel, 3, sizeof(cl_mem), &cl_find_key_list);
+    error = clSetKernelArg(kernel, 4, sizeof(int),    &node_count);
+    
+    global_size = node_count;
+    error = clGetKernelWorkGroupInfo(kernel, device_id, 
+                                     CL_KERNEL_WORK_GROUP_SIZE, 
+                                     sizeof(local_size), &local_size, nullptr);
+    local_size = min(local_size, global_size);
+    
+    start_timer();
+    // <editor-fold defaultstate="collapsed" desc="clEnqueueNDRangeKernel">
+    error = clEnqueueNDRangeKernel(command_queue, kernel, 1, nullptr,
+                                   &global_size, &local_size, 0, nullptr,
+                                   nullptr);
+    clFinish(command_queue);
+    // </editor-fold>
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST GPU clEnqueueNDRangeKernel time: %lld\n", dump_timer_delta());
+    
+    start_timer();
+    // <editor-fold defaultstate="collapsed" desc="clEnqueueReadBuffer">
+    error = clEnqueueReadBuffer(command_queue, cl_find_result_list, CL_TRUE, 0,
+                                sizeof (int) * BST_NODE_COUNT, find_result_list,
+                                0, nullptr, nullptr);
+    // </editor-fold>
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST GPU clEnqueueReadBuffer time: %lld\n", dump_timer_delta());
+    
+    /**
+     * GPU OpenCL warp no branch grouping traverse.
+     */
+    for (int i = 0; i < BST_NODE_COUNT; i++) {
+        find_key_list[i] = (int) ((float) i / local_size * BST_NODE_COUNT);
+    }
+    
+    start_timer();
+    // <editor-fold defaultstate="collapsed" desc="clEnqueueWriteBuffer">
+    error = clEnqueueWriteBuffer(command_queue, cl_node_list, CL_TRUE, 0,
+                                 sizeof (BSTNode) * BST_NODE_COUNT, node_list,
+                                 0, nullptr, nullptr);
+    error = clEnqueueWriteBuffer(command_queue, cl_find_key_list, CL_TRUE, 0,
+                                 sizeof (int) * BST_NODE_COUNT, find_key_list,
+                                 0, nullptr, nullptr);
+    // </editor-fold>
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST GPU clEnqueueWriteBuffer time: %lld\n", dump_timer_delta());
+    
+    node_count = BST_NODE_COUNT;
+    error = clSetKernelArg(kernel, 0, sizeof(cl_mem), &cl_node_list);
+    error = clSetKernelArg(kernel, 1, sizeof(int),    &node_count);
+    error = clSetKernelArg(kernel, 2, sizeof(cl_mem), &cl_find_result_list);
+    error = clSetKernelArg(kernel, 3, sizeof(cl_mem), &cl_find_key_list);
+    error = clSetKernelArg(kernel, 4, sizeof(int),    &node_count);
+    
+    global_size = node_count;
+    error = clGetKernelWorkGroupInfo(kernel, device_id, 
+                                     CL_KERNEL_WORK_GROUP_SIZE, 
+                                     sizeof(local_size), &local_size, nullptr);
+    local_size = min(local_size, global_size);
+    
+    start_timer();
+    // <editor-fold defaultstate="collapsed" desc="clEnqueueNDRangeKernel">
+    error = clEnqueueNDRangeKernel(command_queue, kernel, 1, nullptr,
+                                   &global_size, &local_size, 0, nullptr,
+                                   nullptr);
+    clFinish(command_queue);
+    // </editor-fold>
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST GPU clEnqueueNDRangeKernel time: %lld\n", dump_timer_delta());
+    
+    start_timer();
+    // <editor-fold defaultstate="collapsed" desc="clEnqueueReadBuffer">
+    error = clEnqueueReadBuffer(command_queue, cl_find_result_list, CL_TRUE, 0,
+                                sizeof (int) * BST_NODE_COUNT, find_result_list,
+                                0, nullptr, nullptr);
+    // </editor-fold>
+    stop_timer();
+    DEBUG_PRINT(ERR_INFO, "BST GPU clEnqueueReadBuffer time: %lld\n", dump_timer_delta());
+    
+    // Clean up.
+    delete [] node_list;
+    clReleaseMemObject(cl_node_list);
+    clReleaseMemObject(cl_find_key_list);
+    clReleaseMemObject(cl_find_result_list);
+    clReleaseKernel(kernel);
+    clReleaseProgram(program);
+    clReleaseCommandQueue(command_queue);
+    clReleaseContext(context);
+    clReleaseDevice(device_id);
+    
+    return EXIT_SUCCESS;
+}
+
---
+++ hsa_coalescing/opencl_factory.cpp
@@ -0,0 +1,93 @@
+/** 
+ * Copyright (c) 2015 VCO Lab CSIE department of National Taiwan University All
+ * rights reserved.
+ * 
+ * This file is part of OpenCL Binary Search Tree for HSA comparison
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *  - Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 
+ *  - Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include "opencl_factory.hpp"
+
+cl_int           error;
+cl_platform_id   platform_id;
+cl_device_id     device_id;
+cl_context       context;
+cl_command_queue command_queue;
+cl_program       program;
+cl_kernel        kernel;
+
+
+
+void initialize_cl_environment() {
+    
+    error = clGetPlatformIDs(1, &platform_id, nullptr);
+    if (error != CL_SUCCESS)
+        DEBUG_THROW(ERR_SYSCALL_FAIL, "clGetPlatformIDs error: %d\n", error);
+    
+    error = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, nullptr);
+    if (error != CL_SUCCESS)
+        DEBUG_THROW(ERR_SYSCALL_FAIL, "clGetDeviceIDs error: %d\n", error);
+    
+    char device_name[256];
+    error = clGetDeviceInfo(device_id, CL_DEVICE_NAME, sizeof(device_name), device_name, nullptr);
+    DEBUG_PRINT(ERR_INFO, "Use device name: %s\n", device_name);
+    
+    context = clCreateContext(0, 1, &device_id, nullptr, nullptr, &error);
+    if (error != CL_SUCCESS)
+        DEBUG_THROW(ERR_SYSCALL_FAIL, "clCreateContext error: %d\n", error);
+    
+    command_queue = clCreateCommandQueue(context, device_id, 0, &error);
+    if (error != CL_SUCCESS)
+        DEBUG_THROW(ERR_SYSCALL_FAIL, "clCreateCommandQueue error: %d\n", error);
+    
+}
+
+void load_cl_program_from_file(const char* file_name, const char* kernel_name) {
+
+	fstream file_stream;
+	file_stream.exceptions(ifstream::eofbit | ifstream::failbit | ifstream::badbit);
+	file_stream.open(file_name, std::fstream::in | std::fstream::binary);
+
+	string program_source(istreambuf_iterator<char>(file_stream), (istreambuf_iterator<char>()));
+
+	const char* program_source_buffer = program_source.c_str();
+  
+    program = clCreateProgramWithSource(context, 1, &program_source_buffer, nullptr, &error);
+    if (error != CL_SUCCESS)
+        DEBUG_THROW(ERR_SYSCALL_FAIL, "clCreateProgramWithSource error");
+    
+    error = clBuildProgram(program, 0, nullptr, "-I.", nullptr, nullptr);
+    if (error != CL_SUCCESS) {
+        size_t error_len;
+        char error_buffer[2048];
+        clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, 
+                              sizeof(error_buffer), error_buffer, &error_len
+        );
+        DEBUG_THROW(ERR_SYSCALL_FAIL, "clCreateProgramWithSource %s", error_buffer);
+    }
+    
+    kernel = clCreateKernel(program, kernel_name, &error);
+    if (error != CL_SUCCESS)
+        DEBUG_THROW(ERR_SYSCALL_FAIL, "clCreateKernel error");
+}
